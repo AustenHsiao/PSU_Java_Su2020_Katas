851fae1a2a76dfd84e1543761f949f8694b751c9
Cyrus Rice <cyrus.rice@frontier.com>
2020-07-08 20:17:19 -0700

    Added functionality

--- RomanNumerals/src/main/java/edu/pdx/cs410J/ahsiao/Kata.java
+++ RomanNumerals/src/main/java/edu/pdx/cs410J/ahsiao/Kata.java
@@ -13,4 +13,32 @@ public class Kata {
     System.err.println("Missing command line arguments");
     System.exit(1);
   }
+  public static String decToRoman(int decimal) {
+    String numeral = "";
+    if (decimal > 1000) {
+      for (int i = 0; i < (int)(decimal / 1000); i++) {
+        numeral += "M";
+      }
+    }
+    decimal -= (int)(decimal / 1000) * 1000;
+    if (decimal > 500) {
+      for (int i = 0; i < (int)(decimal / 500); i++) {
+        numeral += "D";
+      }
+    }
+    decimal -= (int)(decimal / 500) * 500;
+    if (decimal > 100) {
+      for (int i = 0; i < (int)(decimal / 100); i++) {
+        numeral += "C";
+      }
+    }
+    decimal -= (int)(decimal / 100) * 100;
+    if (decimal > 50) {
+      for (int i = 0; i < (int)(decimal / 50); i++) {
+        numeral += "L";
+      }
+    }
+    decimal -= (int)(decimal / 50) * 50;
+    return numeral;
+  }
 }
--- RomanNumerals/src/test/java/edu/pdx/cs410J/ahsiao/KataTest.java
+++ RomanNumerals/src/test/java/edu/pdx/cs410J/ahsiao/KataTest.java
@@ -2,12 +2,25 @@ package edu.pdx.cs410J.ahsiao;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class KataTest
 {
 
   @Test
   public void canInstantiateKataClass() {
     new Kata();
+  }
+
+  @Test
+  public void TestOne() {
+    assertThat(Kata.decToRoman(1), equalTo("I"));
+  }
+
+  @Test
+  public void TestFour() {
+    assertThat(Kata.decToRoman(4), equalTo("IV"));
   }
 
 }
