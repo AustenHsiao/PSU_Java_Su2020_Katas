6fd82681cc083db3f886863755a5e6e906cf0c1e
AustenHsiao <Austenhsiao@gmail.com>
2020-07-08 20:46:27 -0700

    .

--- RomanNumerals/src/main/java/edu/pdx/cs410J/ahsiao/Kata.java
+++ RomanNumerals/src/main/java/edu/pdx/cs410J/ahsiao/Kata.java
@@ -14,31 +14,75 @@ public class Kata {
     System.exit(1);
   }
   public static String decToRoman(int decimal) {
+    if(decimal > 3000){
+      return "Too large";
+    }
+
     String numeral = "";
-    if (decimal > 1000) {
+    if (decimal >= 1000) {
       for (int i = 0; i < (int)(decimal / 1000); i++) {
         numeral += "M";
       }
+      decimal -= (int)(decimal / 1000) * 1000;
     }
-    decimal -= (int)(decimal / 1000) * 1000;
-    if (decimal > 500) {
+
+    if(decimal == 900){
+      numeral += "CM";
+      decimal -= 900;
+    }else if(decimal >= 500) {
       for (int i = 0; i < (int)(decimal / 500); i++) {
         numeral += "D";
       }
+      decimal -= (int)(decimal / 500) * 500;
     }
-    decimal -= (int)(decimal / 500) * 500;
-    if (decimal > 100) {
+
+    if(decimal == 400){
+      numeral += "CD";
+      decimal -= 400;
+    } else if(decimal >= 100) {
       for (int i = 0; i < (int)(decimal / 100); i++) {
         numeral += "C";
       }
+      decimal -= (int)(decimal / 100) * 100;
     }
-    decimal -= (int)(decimal / 100) * 100;
-    if (decimal > 50) {
+
+    if(decimal == 90){
+      numeral += "XC";
+      decimal -= 90;
+    }else if(decimal >= 50) {
       for (int i = 0; i < (int)(decimal / 50); i++) {
         numeral += "L";
       }
+      decimal -= (int)(decimal / 50) * 50;
     }
-    decimal -= (int)(decimal / 50) * 50;
+
+    if(decimal == 40){
+      numeral += "XL";
+      decimal -= 40;
+    }else if(decimal >= 10) {
+      for (int i = 0; i < (int)(decimal / 10); i++) {
+        numeral += "X";
+      }
+      decimal -= (int)(decimal / 10) * 10;
+    }
+
+    if(decimal == 9){
+      numeral += "IX";
+      decimal -= decimal - 9;
+    } else if(decimal >= 5) {
+      for (int i = 0; i < (int)(decimal / 5); i++) {
+        numeral += "V";
+      }
+      decimal -= (decimal / 5) * 5;
+    }
+
+    if(decimal == 4){
+      numeral += "IV";
+    }else if (decimal >= 1) {
+      for (int i = 0; i < decimal; i++) {
+        numeral += "I";
+      }
+    }
     return numeral;
   }
 }
--- RomanNumerals/src/test/java/edu/pdx/cs410J/ahsiao/KataTest.java
+++ RomanNumerals/src/test/java/edu/pdx/cs410J/ahsiao/KataTest.java
@@ -23,4 +23,46 @@ public class KataTest
     assertThat(Kata.decToRoman(4), equalTo("IV"));
   }
 
+  @Test
+  public void TestTen() {
+    assertThat(Kata.decToRoman(10), equalTo("X"));
+  }
+
+  @Test
+  public void TestFifty() {
+    assertThat(Kata.decToRoman(50), equalTo("L"));
+  }
+
+  @Test
+  public void TestHundred() {
+    assertThat(Kata.decToRoman(100), equalTo("C"));
+  }
+
+  @Test
+  public void TestFiveHundred() {
+    assertThat(Kata.decToRoman(500), equalTo("D"));
+  }
+
+  @Test
+  public void TestThousand() {
+    assertThat(Kata.decToRoman(1000), equalTo("M"));
+  }
+
+  @Test
+  public void TestThreeThousand() {
+    assertThat(Kata.decToRoman(3000), equalTo("MMM"));
+  }
+
+  @Test
+  public void TestThirtyEight() {
+    assertThat(Kata.decToRoman(38), equalTo("XXXVIII"));
+  }
+
+  @Test
+  public void TestFourtyNine() {
+    assertThat(Kata.decToRoman(49), equalTo("XLIX"));
+  }
+
+
+
 }
